1b4eb3c46ffe3f6cb5ce3613e556e30b5be4210e
nesbitm <mlnesbit98@gmail.com>
2021-06-02 11:33:09 +0100

    built new toc

---
+++ content/_build/jupyter_execute/notebooks/6-Bootstrap_rTPC_tutorial_Nesbit.r
@@ -0,0 +1,129 @@
+library(repr) ; options(repr.plot.width = 5, repr.plot.height = 6) # Change plot sizes (in cm)
+
+require('ggplot2')
+require('nls.multstart')
+require('broom')
+require('tidyverse')
+require('rTPC')
+require('dplyr')
+require('data.table')
+require('car')
+require('boot')
+require('patchwork')
+require('minpack.lm')
+require("tidyr")
+require('purrr')
+# update.packages(ask = FALSE)
+
+rm(list=ls())
+graphics.off()
+setwd("/home/primuser/Documents/VByte/VecMismatchPaper1/code/")
+
+#take a look at the different models available
+get_model_names()
+
+#read in the trait data
+final_trait_data <- read.csv('../data/Final_Traitofinterest.csv')
+
+df1 <- final_trait_data %>%
+  dplyr::select('originalid', 'originaltraitname', 'originaltraitunit', 'originaltraitvalue', 'interactor1', 'ambienttemp', 'citation')
+#filter to single species and trait
+df2 <- dplyr::filter(df1, originalid == 'csm7I')
+
+#visualize
+ggplot(df, aes(ambienttemp, originaltraitvalue))+
+  geom_point()+
+  theme_bw(base_size = 12) +
+  labs(x = 'Temperature (ºC)',
+       y = 'Development Rate',
+       title = 'Development Rate across temperatures for Aedes albopictus')
+
+# choose model
+mod = 'sharpschoolhigh_1981'
+d<- df2 %>%
+  rename(temp = ambienttemp,
+         rate = originaltraitvalue)
+
+# fit Sharpe-Schoolfield model
+d_fit <- nest(d, data = c(temp, rate)) %>%
+  mutate(sharpeschoolhigh = map(data, ~nls_multstart(rate~sharpeschoolhigh_1981(temp = temp, r_tref,e,eh,th, tref = 15),
+                                                     data = .x,
+                                                     iter = c(3,3,3,3),
+                                                     start_lower = get_start_vals(.x$temp, .x$rate, model_name = 'sharpeschoolhigh_1981') - 10,
+                                                     start_upper = get_start_vals(.x$temp, .x$rate, model_name = 'sharpeschoolhigh_1981') + 10,
+                                                     lower = get_lower_lims(.x$temp, .x$rate, model_name = 'sharpeschoolhigh_1981'),
+                                                     upper = get_upper_lims(.x$temp, .x$rate, model_name = 'sharpeschoolhigh_1981'),
+                                                     supp_errors = 'Y',
+                                                     convergence_count = FALSE)),
+         
+         # create new temperature data
+         new_data = map(data, ~tibble(temp = seq(min(.x$temp), max(.x$temp), length.out = 100))),
+         # predict over that data,
+         preds =  map2(sharpeschoolhigh, new_data, ~augment(.x, newdata = .y)))
+
+# unnest predictions
+d_preds <- select(d_fit, preds) %>%
+  unnest(preds)
+
+# plot data and predictions
+ggplot() +
+  geom_line(aes(temp, .fitted), d_preds, col = 'blue') +
+  geom_point(aes(temp, rate), d, size = 2, alpha = 0.5) +
+  theme_bw(base_size = 12) +
+  labs(x = 'Temperature (ºC)',
+       y = 'Growth rate',
+       title = 'Growth rate across temperatures')
+
+# refit model using nlsLM
+fit_nlsLM <- minpack.lm::nlsLM(rate~sharpeschoolhigh_1981(temp = temp, r_tref,e,eh,th, tref = 15),
+                               data = d,
+                               start = coef(d_fit$sharpeschoolhigh[[1]]),
+                               lower = get_lower_lims(d$temp, d$rate, model_name = 'sharpeschoolhigh_1981'),
+                               upper = get_upper_lims(d$temp, d$rate, model_name = 'sharpeschoolhigh_1981'),
+                               weights = rep(1, times = nrow(d)))
+
+# bootstrap using case resampling
+boot1 <- Boot(fit_nlsLM, method = 'case')
+
+# look at the data
+head(boot1$t)
+
+hist(boot1, layout = c(2,2))
+
+# create predictions of each bootstrapped model
+boot1_preds <- boot1$t %>%
+  as.data.frame() %>%
+  drop_na() %>%
+  mutate(iter = 1:n()) %>%
+  group_by_all() %>%
+  do(data.frame(temp = seq(min(d$temp), max(d$temp), length.out = 100))) %>%
+  ungroup() %>%
+  mutate(pred = sharpeschoolhigh_1981(temp, r_tref, e, eh, th, tref = 15))
+
+# calculate bootstrapped confidence intervals
+boot1_conf_preds <- group_by(boot1_preds, temp) %>%
+  summarise(conf_lower = quantile(pred, 0.025),
+            conf_upper = quantile(pred, 0.975)) %>%
+  ungroup()
+
+# plot bootstrapped CIs
+p1 <- ggplot() +
+  geom_line(aes(temp, .fitted), d_preds, col = 'blue') +
+  geom_ribbon(aes(temp, ymin = conf_lower, ymax = conf_upper), boot1_conf_preds, fill = 'blue', alpha = 0.3) +
+  geom_point(aes(temp, rate), d, size = 2, alpha = 0.5) +
+  theme_bw(base_size = 12) +
+  labs(x = 'Temperature (ºC)',
+       y = 'Growth rate',
+       title = 'Growth rate across temperatures')
+
+# plot bootstrapped predictions
+p2 <- ggplot() +
+  geom_line(aes(temp, .fitted), d_preds, col = 'blue') +
+  geom_line(aes(temp, pred, group = iter), boot1_preds, col = 'blue', alpha = 0.007) +
+  geom_point(aes(temp, rate), d, size = 2, alpha = 0.5) +
+  theme_bw(base_size = 12) +
+  labs(x = 'Temperature (ºC)',
+       y = 'Growth rate',
+       title = 'Growth rate across temperatures')
+
+p1 + p2
